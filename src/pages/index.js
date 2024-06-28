132b458c103db3f28c0527c4556c0d0036addfc5
Tom Walsh <hello@tmwlsh.co.uk>
2024-06-28 23:03:31 +0100

    text changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,17 +7,17 @@ import Interests from "../components/interests";
 export const Head = () => {
   return (
     <>
-      <title>TMWLSH - UX Consultancy</title>
-      <meta name="title" content="TMWLSH - UX Consultancy" />
-      <meta name="description" content="TMWLSH is a UX Consultancy focussed on creating usable, enjoyable, and accessible experiences for clients of all shapes and sizes." />
+      <title>TMWLSH - Designer & Developer</title>
+      <meta name="title" content="TMWLSH - Designer & Developer" />
+      <meta name="description" content="A Senior Frontend / Shopify Developer and UX consultant. With 14+ years of digital experience" />
       <meta name="keywords" content="UX, UI, UX Consultancy, UX Consultancy Leeds, UX Consultancy Kendal, UX Consultancy London" />
       <meta name="robots" content="index, follow" />
       <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
       <meta name="language" content="English" />
-      <meta property="og:title" content="TMWLSH - UX Consultancy" />
+      <meta property="og:title" content="TMWLSH - Designer & Developer" />
       <meta property="og:site_name" content="TMWLSH" />
       <meta property="og:url" content="www.northpeak.digital" />
-      <meta property="og:description" content="TMWLSH is a UX Consultancy focussed on creating usable, enjoyable, and accessible experiences for clients of all shapes and sizes." />
+      <meta property="og:description" content="A Senior Frontend / Shopify Developer and UX consultant. With 14+ years of digital experience" />
       <meta property="og:type" content="website" />
       <meta property="og:image" content="/share-image.jpg" />
     </>
